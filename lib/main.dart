5b41b38cbfb7b37d2c235db938e60eabd3978638
DGadonski <derek.gadonski@gmail.com>
2023-08-30 11:27:26 -0300

    Create method 'salvarArquivo'.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, library_prefixes
 
 import 'package:flutter/material.dart';
 import 'package:lista_tarefas/Home.dart';
+// import 'dart:ui_web' as uiWeb;
+
 
 void main() {
+
+  // uiWeb.debugEmulateFlutterTesterEnvironment=false;
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Home()
